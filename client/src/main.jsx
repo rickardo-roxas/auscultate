1dc8b5cb7cc147adafe2947758417663826a7232
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-05-02 22:16:54 +0800

    Fixed configurations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Libraries and dependencies
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
+// Global styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/styles/Styles.css";
+
+// Main app
+import App from './App.jsx';
+
+/**
+ * Main entry point for the React application.
+ */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
