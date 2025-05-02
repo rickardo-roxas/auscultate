283eb37a2040daac21a4b227ba39402784bc0b09
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-05-02 22:08:08 +0800

    Fixed entry point of client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
 
+// Pages and Components
+import Wrapper from './components/layout/Wrapper';
+import ErrorBoundary from './components/ui/ErrorBoundary/ErrorBoundary';
+import HomePage from './pages/HomePage';
+import HowItWorksPage from './pages/HowItWorksPage';
+import ResourcesPage from './pages/ResourcesPage';
+
+/**
+ * Main application component.
+ * @returns {JSX.Element} - The main application component.
+ */
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <Wrapper>
+        <Routes>
+          <Route path='/' element={
+            <ErrorBoundary fallback={<div>Oops! Failed to load this page.</div>}>
+              <HomePage />
+            </ErrorBoundary>}
+          />
+          <Route path='/how-it-works' element={
+            <ErrorBoundary fallback={<div>Oops! Failed to load this page.</div>}>
+              <HowItWorksPage />
+            </ErrorBoundary>}
+          />
+          <Route path='/resources' element={
+            <ErrorBoundary fallback={<div>Oops! Failed to load this page.</div>}>
+              <ResourcesPage />
+            </ErrorBoundary>}
+          />
+        </Routes>
+      </Wrapper>
+    </Router>
+  );
 }
 
-export default App
+export default App;
