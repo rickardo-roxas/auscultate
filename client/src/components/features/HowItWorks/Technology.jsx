526f53886735474b927b655ab7a266dba3f4b52e
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2025-05-04 14:53:36 +0800

    Added technology info component for How It Works page

---
+++ client/src/components/features/HowItWorks/Technology.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Technology.module.css';
+
+function Technology() {
+  return (
+    <section className={styles.techSection}>
+      <h2>Auscultate Technology</h2>
+      <p> 
+        To replace: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+    </section>
+  );
+}
+
+export default Technology;
