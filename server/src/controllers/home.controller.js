6d62d096ebc4d422ca0fac223e1b99238877b979
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-05-06 09:39:23 +0800

    Added controllers

---
+++ server/src/controllers/home.controller.js
@@ -0,0 +1,23 @@
+import ResponseHandler from '../utils/responseHandler.js';
+
+/**
+ * @description - Controller for handling home page requests.
+ * @param {*} req - The request object
+ * @param {*} res - The response object
+ * @param {*} next - The next middleware function
+ */
+const index = async (req, res, next) => {
+    try {
+        ResponseHandler.success(res, {
+            status: 200,
+            message: 'Connected to server. Ready for file upload.',
+        });
+    } catch (err) {
+        err.statusCode = 500;
+        next(err);
+    }
+}
+
+export default {
+    index,
+}
---
+++ server/src/controllers/predict.controller.js
@@ -0,0 +1,33 @@
+import ModelService from "../services/model.service.js";
+import ResponseHandler from "../utils/responseHandler.js";
+
+/**
+ * Handles the prediction result.
+ * @param {*} req - The request object
+ * @param {*} res - The response object 
+ * @param {*} next - The next middleware function
+ * @returns {Promise<void>} - Sends a JSON response with the prediction result.
+ */
+const getResult = async (req, res, next) => {
+    try {
+        const result = await ModelService.predict();
+
+        if (!result) {
+            return ResponseHandler.errorResponse(res, {
+                status: 400,
+                message: 'No prediction available. Please upload an audio file with respiratory cycles.'
+            });
+        }
+        return ResponseHandler.successResponse(res, {
+            message: 'Prediction retrieved successfully.',
+            data: result,
+        })
+    } catch (err) {
+        err.statusCode = 500;
+        next(err)
+    }
+}
+
+export default {
+    getResult,
+}
