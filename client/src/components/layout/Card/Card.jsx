ee6e3accdea574dd45cd21dd67d1b332c00720bc
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-05-10 07:41:52 +0800

    Fixed bug in card

--- client/src/components/layout/Card/Card.jsx
+++ client/src/components/layout/Card/Card.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import {Card as BootstrapCard} from 'react-bootstrap';
+import styles from './Card.module.css';
 
-function Card(children, styles='') {
+/**
+ * @description Reusable Card component that renders a card.
+ * @param {*} children - Card body content. Can contain multiple elements.
+ * @param {*} className - Optional additional class name for the card.
+ * @returns {JSX.Element} - Rendered card component
+ * 
+ * @example
+ * <Card>
+ *    <h2>Card Title</h2>
+ *   <p>Card content goes here.</p>
+ * </Card>
+ * 
+ * @example
+ * <Card className="custom-class">
+ *   <h2>Card Title</h2>
+ *  <p>Card content goes here.</p>
+ * </Card>
+ */
+function Card({children, className=''}) {
     return (
         <BootstrapCard className={`${styles.card} ${className}`}>
             <BootstrapCard.Body className={styles.body}>
