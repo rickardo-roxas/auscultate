5d601e63debce90857c8a566685d316a82881d9d
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-05-13 23:33:48 +0800

    Fixed path

--- model-server/app/models/model_loader.py
+++ model-server/app/models/model_loader.py
@@ -1,5 +1,5 @@
 from keras.models import load_model
-from configs.model_config import MODEL_PATH
+from configs.config import MODEL_PATH
 
 print(f"[INFO] Loading model from: {MODEL_PATH}")
 model = load_model(MODEL_PATH)
