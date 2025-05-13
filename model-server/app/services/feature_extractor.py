a8ab48c5cd56ca96b10653ec852e2206a07d04ba
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2025-05-13 23:34:04 +0800

    Added sampling rate

--- model-server/app/services/feature_extractor.py
+++ model-server/app/services/feature_extractor.py
@@ -5,7 +5,7 @@ from fastapi import UploadFile
 
 def extract_features_from_upload(file: UploadFile):
     audio_bytes = io.BytesIO(file.file.read())
-    y, sr = librosa.load(audio_bytes, sr=None)
+    y, sr = librosa.load(audio_bytes, sr=22050)
 
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
     chroma = librosa.feature.chroma_stft(y=y, sr=sr)
